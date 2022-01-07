ec839167c327a822706f6b2f43cf0375424ae3f6
tyler fretz <fretztyler@gmail.com>
2022-01-07 14:16:11 -0500

    Set up base server

---
+++ index.ts
@@ -0,0 +1,34 @@
+require('dotenv').config();
+import path from 'path';
+import express from 'express';
+import mongoose from 'mongoose';
+require('express-async-errors');
+import cors from 'cors';
+const helmet = require('helmet');
+import compression from 'compression';
+
+const app = express();
+
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(compression());
+	mongoose.connect(process.env.MONGO_URI ?? '')
+		.then(() => console.log('Connected to database'))
+		.catch(err => console.error(err));
+}
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/client/build'));
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${process.env.PORT}`);
+});
