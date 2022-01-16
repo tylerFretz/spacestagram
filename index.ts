e6b003a9f0fd1a97a9d74b9ae47d8d7654fcde27
tyler fretz <fretztyler@gmail.com>
2022-01-16 13:12:07 -0500

    trying to make heroku happy

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 require('express-async-errors');
 const cors = require('cors');
 const compression = require('compression');
+const helmet = require('helmet');
 const postRouter = require('./controllers/postController');
 
 const app = express();
 
+app.use(helmet({
+	contentSecurityPolicy: false
+}));
+app.use(helmet.hidePoweredBy());
 app.use(cors());
 app.use(express.json());
 
@@ -22,13 +28,14 @@ else {
 		.then(() => console.log('Connected to database'))
 		.catch(err => console.error(err));
 }
+console.log(path.join(__dirname, './client/build'));
 
-app.use(express.static('./client/build'));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use('/api/posts', postRouter);
 
 app.get('/*', (req, res) => {
-	res.sendFile('./client/build');
+	res.sendFile(path.join(__dirname, './client/build'));
 });
 
 const PORT = process.env.PORT || 3001;
